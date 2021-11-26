be130be8b1f6692bfffd0279157f053f51f4de4f
German Arutyunov <garutyunov@tenderhub.ru>
2021-11-26 13:30:06 +0300

    README.md

--- internal/task.go
+++ internal/task.go
@@ -1,1 +1,19 @@
 package internal
+
+import (
+	"fmt"
+	"os"
+)
+
+func PrintTask() error {
+	r, err := os.ReadFile("README.md")
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println()
+	fmt.Println(string(r))
+
+	return nil
+}
